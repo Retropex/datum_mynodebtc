233c91a9490d2b31d60234fcb8753d14ece6bd4b
Léo Haf <leohaf@orangepill.ovh>
2025-03-18 17:18:24 +0100

    update dummy address

--- scripts/install_datum.sh
+++ scripts/install_datum.sh
@@ -30,40 +30,27 @@ echo "{
   \"bitcoind\": {
     \"rpcuser\": \"mynode\",
     \"rpcpassword\": \"auto-config\",
-    \"rpcurl\": \"127.0.0.1:8332\",
-    \"work_update_seconds\": 40
+    \"rpcurl\": \"127.0.0.1:8332\"
   },
   \"api\": {
-    \"listen_port\": 21000
+    \"listen_port\": 21000,
+    \"modify_conf\": true,
+    \"admin_password\": \"bolt\"
   },
   \"mining\": {
-    \"pool_address\": \"1111111111111111111114oLvT2\",
+    \"pool_address\": \"enter_your_bitcoin_address\",
     \"coinbase_tag_primary\": \"DATUM on mynode\",
-    \"coinbase_tag_secondary\": \"DATUM on mynode\",
-    \"coinbase_unique_id\": 120
+    \"coinbase_tag_secondary\": \"DATUM on mynode\"
   },
   \"stratum\": {
-    \"listen_port\": 23334,
-    \"max_clients_per_thread\": 1000,
-    \"max_threads\": 8,
-    \"max_clients\": 2048,
-    \"vardiff_min\": 16384,
-    \"vardiff_target_shares_min\": 8,
-    \"vardiff_quickdiff_count\": 8,
-    \"vardiff_quickdiff_delta\": 8,
-    \"share_stale_seconds\": 120,
-    \"fingerprint_miners\": true
+    \"listen_port\": 23334
   },
   \"logger\": {
     \"log_level_console\": 2
   },
   \"datum\": {
-    \"pool_host\": \"datum-beta1.mine.ocean.xyz\",
-    \"pool_port\": 28915,
-    \"pool_pubkey\": \"f21f2f0ef0aa1970468f22bad9bb7f4535146f8e4a8f646bebc93da3d89b1406f40d032f09a417d94dc068055df654937922d2c89522e3e8f6f0e649de473003\",
     \"pool_pass_workers\": true,
     \"pool_pass_full_users\": true,
-    \"always_pay_self\": true,
     \"pooled_mining_only\": true
   }
 }
